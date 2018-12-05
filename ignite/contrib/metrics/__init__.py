250c898346b8f6ce501a84fe6e86b15c0fcc89e8
Adrian Borucki <a.borucki@samsung.com>
2018-12-05 20:36:16 +0100

    Add Hamming Loss metric

--- ignite/contrib/metrics/__init__.py
+++ ignite/contrib/metrics/__init__.py
@@ -1,2 +1,3 @@
 from ignite.contrib.metrics.average_precision import AveragePrecision
 from ignite.contrib.metrics.roc_auc import ROC_AUC
+from ignite.contrib.metrics.hamming_loss import HammingLoss
